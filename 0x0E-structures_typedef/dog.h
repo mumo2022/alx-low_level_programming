597645d14d7eb26cdb33b1721637a616c22ec891
Mumo <mumofrancis94@gmail.com>
2023-03-21 08:19:26 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog elements
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description - members of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
